5188ca1d60dde705aee5bb5d78d38c9fe614026d
Sanniv <sanniv.1020@gmail.com>
2024-05-02 16:58:17 +0530

    changes

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const courses_1 = __importDefault(require("./courses"));
+const CORS = require("cors");
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
+app.use(CORS());
+app.use(express_1.default.json());
 app.get("/", (_, res) => {
     res.json({
         message: "Server is healthy"
